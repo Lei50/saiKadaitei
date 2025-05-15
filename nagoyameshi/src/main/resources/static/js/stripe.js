a23b926cec19eacf180fec4d702072b273fe0a7d
Lei50 <shishirarure826@gmail.com>
2025-05-15 22:39:14 +0900

    提出

--- nagoyameshi/src/main/resources/static/js/stripe.js
+++ nagoyameshi/src/main/resources/static/js/stripe.js
@@ -1,4 +1,4 @@
-const stripe = Stripe(pk_test_51RDu3mBUES6M2scHCPkoIpggmDCv2ScRA1aIUYQYea4oKYuTZzhcMQosbZ4dWmQ1a0IbjCJpAGNs3iy10KxUAbdP00Uqrs5eaV);
+const stripe = Stripe("pk_test_51RDu3mBUES6M2scHCPkoIpggmDCv2ScRA1aIUYQYea4oKYuTZzhcMQosbZ4dWmQ1a0IbjCJpAGNs3iy10KxUAbdP00Uqrs5eaV");
 
 const elements = stripe.elements();
 const cardElement = elements.create('card');

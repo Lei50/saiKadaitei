35e26fd75d9a02d8b0fdd900b5c3a6eddf420bfe
Lei50 <shishirarure826@gmail.com>
2025-05-19 23:19:26 +0900

    Fix footer overlapping member list

--- nagoyameshi/src/main/java/GenderRepository/GenderRepository.java
+++
@@ -1,9 +0,0 @@
-package GenderRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.nagoyameshi.entity.Gender;
-
-public interface GenderRepository extends JpaRepository<Gender, Integer> {
-
-}
--- nagoyameshi/src/main/java/com/example/nagoyameshi/repository/OccupationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.nagoyameshi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.nagoyameshi.entity.Occupation;
-
-public interface OccupationRepository extends JpaRepository<Occupation, Integer> {
-
-}

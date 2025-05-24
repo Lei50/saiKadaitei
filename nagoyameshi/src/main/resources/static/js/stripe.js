e9c13f8afb120473d045621e4dc3c45400d33298
Lei50 <shishirarure826@gmail.com>
2025-05-24 09:01:39 +0900

    Fix footer overlapping member list

--- nagoyameshi/src/main/resources/static/js/stripe.js
+++ nagoyameshi/src/main/resources/static/js/stripe.js
@@ -1,8 +1,9 @@
-const stripe = Stripe('pk_test_51RDu3mBUES6M2scHCPkoIpggmDCv2ScRA1aIUYQYea4oKYuTZzhcMQosbZ4dWmQ1a0IbjCJpAGNs3iy10KxUAbdP00Uqrs5eaV');
+const stripe = Stripe("pk_test_51RDu3mBUES6M2scHCPkoIpggmDCv2ScRA1aIUYQYea4oKYuTZzhcMQosbZ4dWmQ1a0IbjCJpAGNs3iy10KxUAbdP00Uqrs5eaV");
 const paymentButton = document.querySelector('#paymentButton');
- 
- paymentButton.addEventListener('click', () => {
-   stripe.redirectToCheckout({
-     sessionId: sessionId
-   })
- });;
+
+paymentButton.addEventListener('click', () => {
+	stripe.redirectToCheckout({
+		sessionId: sessionId
+	})
+});
+
--- nagoyameshi/src/main/resources/static/js/token.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const token = urlParams.get('token');
-    if (token) {
-        document.getElementById('tokenField').value = token;
-    }
-});
